24e7f10d0b95be001429d986fcbef8b1b8076e8d
Ambreprqt <113826570+Ambreprqt@users.noreply.github.com>
2024-03-05 17:50:19 +0100

    authentification

--- src/main/java/com/clementbayard/clement_ws/SecurityConfig.java
+++ src/main/java/com/clementbayard/clement_ws/SecurityConfig.java
@@ -2,51 +2,24 @@ package com.clementbayard.clement_ws;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        // Créer des utilisateurs en mémoire avec des rôles
-        manager.createUser(User.withUsername("user")
-                .password(bCryptPasswordEncoder.encode("userPass"))
-                .roles("USER")
-                .build());
-
-        manager.createUser(User.withUsername("admin")
-                .password(bCryptPasswordEncoder.encode("adminPass"))
-                .roles("ADMIN")
-                .build());
-
-        return manager;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .antMatchers("/article/**").hasRole("ADMIN") // Autoriser l'accès aux chemins commençant par "/article" pour le rôle "ADMIN"
-                                .anyRequest().permitAll()
-                )
-                .httpBasic(withDefaults());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.build();
     }
 }
+
